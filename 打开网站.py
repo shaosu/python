e2c39833ae72572f9885a7664324ce93a5405b01
shaosu <1763319552@qq.com>
2016-02-23 11:09:50 +0800

    打开网站
    打开网站

---
+++ 打开网站.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+#打开网站
+import sys
+import subprocess
+
+keywords = sys.argv[1:]
+
+url = "https://s.taobao.com/search?q="
+for i in keywords:
+	url += i + "+"
+url = url[:-1] 
+subprocess.call(["open",url])
